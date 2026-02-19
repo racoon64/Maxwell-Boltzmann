af93a288e948e532f313755a697d43b46c28590f
racoon64 <rconst8@icloud.com>
2026-02-19 12:24:56 -0500

    Update Equation_website9.py

--- Equation_website9.py
+++ Equation_website9.py
@@ -7,7 +7,7 @@ import io
 
 
 st.title("Maxwell-Boltzmann Speed Distribution")
-st.write("The Maxwell-Boltzmann speed distribution function is a probability density function that describes the distribution of particle speeds in an ideal gas. The shape of this curve depends on two key parameters: the absolute temperature of the gas and the molar mass of the particles.")
+st.write("The Maxwell-Boltzmann speed distribution function is a probability density function that describes the distribution of particle speeds in an ideal gas. The shape of this curve depends on two key parameters: the absolute temperature of the gas and the molar mass of the particles. The function is modeled by the equation:")
 st.latex(r"f(v) = 4\pi \left( \frac{m}{2\pi k_B T} \right)^{3/2} v^2 \exp\left( -\frac{m v^2}{2 k_B T} \right)")
 st.header("RMS Speed")
 st.write("The RMS speed is calculated by multiplying the Maxwell-Boltzmann probability density function by velocity squared ($v^2$), integrating from zero to infinity, and taking the square root. The definition is:")
